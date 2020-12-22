597cad550c7793a9dda0abfb22c1530c3b31503a
lzj <1476673483@qq.com>
2020-12-22 20:54:12 +0800

    输出右边的数大于本身的所有数字

---
+++ cmp.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//int main() {
+//	int a[20];
+//	int i;
+//	for (i = 0; i < 20; i++) {
+//			a[i] = rand() % 100;
+//			}
+//	for (i = 0; i < 20; i++) {
+//		printf("%d ", a[i]);
+//		if ((i + 1) % 5 == 0)
+//		printf("\n");	
+//	}
+//	for (i = 0; i < 19; i++) {
+//		if (a[i] < a[i + 1]) {
+//			printf("%d ", a[i]);
+//		}
+//	}
+//	return 0;
+//}//输出右边的数大于本身的所有数字
