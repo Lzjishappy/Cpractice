3487724cab767e3ad71ae68006fb7daf01660535
lzj <1476673483@qq.com>
2020-12-22 21:16:04 +0800

    输出字符串中字母个数

---
+++ letternum.c
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+
+////void main() {
+////	char c[80];
+////	gets(c);
+////	int cout = 0;
+////	for (int i = 0; i < strlen(c); i++) {
+////		if (c[i] >= 'a'&&c[i] <= 'z' || c[i] >= 'A'&&c[i] <= 'Z')
+////			cout++;
+////	}
+////	printf("%d", cout);
+////
+////}//输出字符串中字母的个数
