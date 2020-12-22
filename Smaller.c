21978a8ca91f3d745d6f620054887b4903298b29
lzj <1476673483@qq.com>
2020-12-22 20:37:01 +0800

    字符串大写变小写

---
+++ Smaller.c
@@ -0,0 +1,11 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+
+///void main() {
+////	char x;
+////	scanf_s("%c", &x);
+////	x = (x >= 'A'&&x <= 'Z') ? (x + 32) : x;
+////	//x = (x >= 'a'&&x <= 'z') ? (x - 32) : x;
+////	printf("%c", x);
+////}//将输入的大写字母转为小写
+//
