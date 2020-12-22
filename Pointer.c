c000cf0f556c4e3694ed2895ef07246663887e8b
lzj <1476673483@qq.com>
2020-12-22 20:35:55 +0800

    初级指针

---
+++ Pointer.c
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//int main() {//指针
+//
+//	int a=10;
+//	int* p = &a;//取地址
+//	printf("%p ", &a);//输出a的地址
+//	*p = 20;//解引用
+//	printf("%d ", a);
+//	printf("%p ", &a);
+//	printf("%d", sizeof(p));
+//	return 0;
+//}
