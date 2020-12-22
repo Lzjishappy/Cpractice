c39673e57cccd1e9bcc013a7a68711f14af98f31
lzj <1476673483@qq.com>
2020-12-22 21:15:44 +0800

    字符串倒序函数

---
+++ StringReverse.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//
+//void reverse(char s[]) {
+//	int i, j;
+//	char c;
+//	for (i = 0, j = strlen(s) - 1 - i; i < j; i++, j--)
+//	{
+//		c = s[i];
+//		s[i] = s[j];
+//		s[j] = c;	
+//	}
+//}
+//int main() {
+//	char a[80];
+//	gets(a);
+//	reverse(a);
+//	puts(a);
+//	return 0;
+//}//×Ö·û´®µ¹Ðòº¯Êý
