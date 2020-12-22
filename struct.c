5bb939226dea7e04cc4f078fa5ee0e527bc78753
lzj <1476673483@qq.com>
2020-12-22 20:45:11 +0800

    结构体简单示例

---
+++ struct.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//struct Person {
+//	char name[20];//定义变量
+//	short age;
+//	char like[20];
+//};//结构体来描述一个事物
+//
+//int main() {
+//	struct Person p1={"李哲嘉",21,"打篮球" };//变量赋值
+//	printf("姓名是:%s ", p1.name);//输出
+//	p1.age = 22;//修改p1里面的内容
+//	printf("年龄：%d ", p1.age);
+//
+//	struct Person* pb = &p1;//指针pb指向p1的地址
+//	printf("%s ",(*pb).like);//通过指针输出，略微繁琐
+//	printf("%s ", pb->like);// ->的使用
+//	return 0;) {
+//      
+//}
