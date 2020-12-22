56fade9ce94e21ccbf3831a7a195641953f2c4b2
lzj <1476673483@qq.com>
2020-12-22 20:48:23 +0800

    一元钱分配方案

---
+++ OneDollar.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+//int main() {
+//	int n = 0;
+//	for (int i = 1; i <= 100; i++) {
+//		for (int j = 1; j <= 20; j++) {
+//			for (int k = 1; k <= 50; k++) {
+//				if (i + j * 5 +k*2== 100) {
+//					printf("一分%d,二分%d, 五分%d\n", i, k,j);
+//					n++;
+//				}
+//			}	
+//		}
+//	}
+//	printf("一共有%d方法", n);
+//
+//	return 0;
+//}一元钱如何分为一分 二分 五分
