f13fd957c13a1748eff6bf9ea7838565ec20a2a7
lzj <1476673483@qq.com>
2020-12-22 21:29:30 +0800

    求1平均值和与平均值差距最小的数

---
+++ ave.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+////int main() {
+////	float a[5],b[5];
+////	float ave,sum=0;
+////	int imin=0;
+////	for (int i = 0; i < 5; i++) {
+////		scanf("%f", &a[i]);
+////		sum += a[i];
+////	}
+////	printf("%f\n", sum);
+////	ave = sum / 5;
+////	printf("%f\n", ave);
+////	for (int i = 0; i < 5; i++) {
+////		b[i] = fabs(a[i] - ave);
+////	}
+////	for (int i = 0; i < 4; i++) {
+////		if (b[i] > b[i + 1])
+////			imin = i + 1;
+////	}
+////	printf("%f",a[imin]);
+////	return 0;
+////}//求10个数的平均值和与平均值差距最小的数
