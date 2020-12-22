91e7615e7414ba6e62be23a56b5e57f5cb8349c7
lzj <1476673483@qq.com>
2020-12-22 20:30:52 +0800

    简易计算器

---
+++ Calculator.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+//int main() {
+//	float a, b;
+//	char c;
+//	a:
+//	scanf_s("%f", &a);
+//	scanf_s("%c", &c);
+//	scanf_s("%f", &b);
+//	
+//	switch (c)
+//	{
+//	   case '+': printf("%f %c %f=%f\n", a, c, b, a + b); break;
+//		case '-': printf("%f %c %f=%f\n", a, c, b, a -b); break;
+//		case '*': printf("%f %c %f=%f\n", a, c, b, a * b); break;
+//		case '/': printf("%f %c %f=%f\n", a, c, b, a / b); break;
+//	}
+//	goto a;
+//	return 0;
+//}//¼òÒ×¼ÆËãÆ÷
