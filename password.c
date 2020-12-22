58655316c349e84332a186d0abb2c11032b157d2
lzj <1476673483@qq.com>
2020-12-22 20:42:30 +0800

    Password

---
+++ password.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//
+////int main() {
+//	char password[80];
+//	int i = 1;
+//	while (i < 4) {
+//		printf("请输入密码：");
+//		gets(password);
+//		if (strcmp(password, "abc123456") == 0) {
+//			printf("输入密码正确！\n"); break;
+//		}
+//		else {
+//			printf("请重新输入密码，你还有%d次机会，不然将会关闭\n", 3 - i);
+//		}
+//		i++;
+//	}
+//	if (i >= 4) {
+//		system("exit");
+//	}
+//}输入3次密码
+//
---
+++ password1.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//int main() {
+//	int ret = 0;
+//	char password[20];
+//	printf("请输入密码：\n");
+//	gets(password);
+//	
+//	printf("请确认：Y/N\n");
+//	ret = getchar();
+//	if (ret == 'Y') {
+//		printf("确认成功");
+//	}
+//	else
+//	{
+//		printf("确认失败");
+//	}
+//
+//	return 0;
+//
+//}输入密码
