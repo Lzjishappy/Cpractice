2020385338822ccf66afdbd84284392f0073eea8
lzj <1476673483@qq.com>
2020-12-22 20:39:32 +0800

    精确除法

---
+++ Division.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//
+//int main() {
+//	int a, b;
+//	a:
+//	scanf_s("%d", &a);
+//	scanf_s("%d", &b);
+//	if (a%b == 0) {
+//		printf("%d", a / b);
+//	}
+//	else
+//	{
+//		printf("%d”‡%d", a / b, a - b*(a / b));
+//	}
+//	goto a;
+//
+//	return 0;
+//}//º∆À„≥˝∑®
