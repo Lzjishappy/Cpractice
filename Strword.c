9e254b1cf4ec7161513822972361ca2f8f21eef2
lzj <1476673483@qq.com>
2020-12-22 20:59:44 +0800

    提取字符串中单词次数

---
+++ Strword.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+////int main() {
+////	char c[80];
+////	gets(c);
+////	int cout = 0;
+////	for (int i = 0; i <=strlen(c) - 1; i++) {
+////		printf("%c", c[i]);
+////	}
+////
+////	for (int i = 0; i < strlen(c) - 1; i++) {
+////		if ((c[i] >= 'A'&&c[i] <= 'z') && (c[i + 1]<'A' || c[i + 1]>'z'))
+////			cout++;
+////		}
+////	if (c[strlen(c) - 1] >= 'A'&&c[strlen(c) - 1] <= 'z')
+////		cout++;
+////	printf("\n有%d个单词", cout);
+////	return 0;
+////}//提取单词次数
