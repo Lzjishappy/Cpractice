648d0f4592ae726712ac6fe06d2b47b2fbe593d4
lzj <1476673483@qq.com>
2020-12-22 21:30:26 +0800

    随机生成100个0-9的数，并且输出这几个数出现的次数

---
+++ findcount.c
@@ -0,0 +1,35 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+////int main() {
+////	int a[10] = {0}, b[100];
+////	int i;
+////	for (i = 0; i < 100; i++) {
+////		b[i] = rand() % 10;
+////	}
+////	for (i = 0; i < 100; i++) {
+////				printf("%d ", b[i]);
+////			if ((i + 1) % 10 == 0)
+////					printf("\n");	
+////			}
+////	for (i = 0; i < 100; i++) {
+////		switch (b[i])
+////		{
+////		case 0:a[0]++; break;
+////		case 1:a[1]++; break;
+////		case 2:a[2]++; break;
+////		case 3:a[3]++; break;
+////		case 4:a[4]++; break;
+////		case 5:a[5]++; break;
+////		case 6:a[6]++; break;
+////		case 7:a[7]++; break;
+////		case 8:a[8]++; break;
+////		case 9:a[9]++; break;
+////		}
+////	}
+////	for (i = 0; i < 10; i++) {
+////		printf("%d ", a[i]);
+////		
+////	}
+////return 0;
+////}//随机生成100个0-9的数，并且输出这几个数出现的次数
