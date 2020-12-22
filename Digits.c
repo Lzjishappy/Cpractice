e99124dfec4b840e5475a3015f7d6f029e186f44
lzj <1476673483@qq.com>
2020-12-22 20:40:38 +0800

    判断整数位数

---
+++ Digits.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+//int main() {
+//	int n;
+//	int num;
+//xx:
+//	
+//	num = 0;
+//		scanf("%d",& n);
+//	
+//	while (n>0)
+//	{
+//		n = n/10;
+//		num++;
+//		}
+//printf("%d", num);
+//	goto xx;
+//return 0;
+//}//判断一个整数数的位数
