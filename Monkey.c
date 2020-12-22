74632a0ba7ecfd22ee436a30ee99e243f1a67827
lzj <1476673483@qq.com>
2020-12-22 20:40:02 +0800

    猴子吃桃问题

---
+++ Monkey.c
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+//
+//int main() {
+//	int n=1;
+//	for (int i=2; i <= 10; i++) {
+//		n = (n + 1) * 2;
+//		printf("第%d天有%d个桃子\n",11 - i, n);
+//
+//	}
+//	return 0;
+//}//猴子有若干个桃，每天吃一半多一个，到第10天想吃的时候还剩一个桃子，求最初有多少个桃
