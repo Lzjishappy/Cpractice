4b4ddef85372fad5c17df2aefb817696f5e07c0b
lzj <1476673483@qq.com>
2020-12-22 20:33:27 +0800

    斐波那契数列

---
+++ Fibonacci.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//
+//#include<stdio.h>
+//int main() {
+//	int a[10000];
+//
+//	a[1] = 1;
+//	a[2] = 1;
+//	for (int i = 1; i <= 18; i++) {
+//		a[i + 2] = a[i] + a[i + 1];
+//		printf("%d  ", a[i + 1]);
+//	}
+//	return 0;
+//}
+////ì³²¨ÄÇÆõÊýÁÐ
