8f906be1a5b56b80345e0226d32ee5ebb172f2f7
lzj <1476673483@qq.com>
2020-12-22 20:35:00 +0800

    阶乘函数

---
+++ Factorial.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//long jc(int k) {//½×³Ëº¯Êý
+//	int s = 1;
+//	for (int i = 1; i <= k; i++) {
+//		s = s * i;
+//	}
+//	return s;
+//}
+//int main() {
+//	double ss=0;
+//	for (int i = 0; i <= 19; i=i+3) {
+//		ss = ss + 1.0/(1+ jc(1 + i));
+//	}
+//	printf("%lf", ss);
+//}//¼ÆËãº¯Êý
