a7a8050d5401ff60e4fa3a61d16ad9296ba4d2b7
lzj <1476673483@qq.com>
2020-12-26 16:01:10 +0800

    数组：三子棋与电脑对战（规范版）

--- game.h
+++ game.h
@@ -1,7 +1,9 @@
 #define _CRT_SECURE_NO_DEPRECATE
+#define ROW 3
+#define COL 3
 void menu();
 void test();
 void game();
 void exit();
-void board(char arr[3][3]);
+void board(char arr[3][3],int row, int col);
 void play(int b[2]);
--- menu.c
+++ menu.c
@@ -1,32 +1,35 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #include<stdio.h>
+#include<time.h>
+#include"game.h"
 void menu() {
 	printf("-----------------------------------------\n");
 	printf("-------------欢迎来到三子棋游戏----------\n");
 	printf("----输入1.开始游戏----------输入2.退出游戏-----\n");
 	printf("-----------------------------------------\n");
 }
-void board(char arr[3][3]) {
-	printf("\n---------\n");
-	for (int i=0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+void board(char arr[ROW][COL],int row,int col) {
+	for (int i=0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
 			printf("%c ", arr[i][j]);
 			printf("|");
 		}
-		printf("\n---------\n");
+		printf("\n");
+		for(int j=0;j<col;j++)
+		printf("---");
+		printf("\n");
 			
 	}
 }
-void play(int b[2], char arr[3][3],int cout) {
+void play(char arr[ROW][COL],int row,int col,int cout) {
 	int m, n;
-	if (cout % 2 == 1)printf("轮到*棋下：\n");
-	else printf("轮到o棋下：\n");
-	X:
+		printf("轮到你（*）下：\n");
+X:
 	printf("请输入下棋的行数: ");
 	scanf("%d",&m);
 	printf("请输入下棋的列数:");
 	scanf("%d",&n);
-	if (m > 3 || m < 1 || n>3 || n < 1) {
+	if (m > row || m < 1 || n>col || n < 1) {
 		printf("请输入正确的行列数：\n");
 		goto X;
 	}
@@ -34,15 +37,25 @@ void play(int b[2], char arr[3][3],int cout) {
 		printf("此处已有棋，请重新下\n");
 		goto X;
 	}
-	
-	b[0] = m;
-	b[1] = n;
+	cout++;
+	arr[m - 1][n - 1] = '*';  
 }
-int judge(char arr[3][3]) {
+void computerplay(char arr[ROW][COL],int row,int col) {
+	int m, n;
+	srand((unsigned)time(NULL));
+	X:
+	m = rand() % row;
+	n = rand() % col;
+	if (arr[m][n]!= ' ') {
+		goto X;
+	}
+	arr[m][n] = 'o';
+}
+int judge(char arr[ROW][COL], int row, int col) {
 	int k = 0, i,j,find=1;
 	if (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0] && arr[0][2] != ' ')
 		k = 1;
-	for ( i = 0; i < 3; i++) {
+	for ( i = 0; i < row; i++) {
 		
 		if((arr[i][0]==arr[i][1])&& (arr[i][0]==arr[i][2])&& (arr[i][0]!=' '))
 			k = 1;	break;
@@ -51,8 +64,8 @@ int judge(char arr[3][3]) {
 		if (arr[i][i] == arr[i + 1][i + 1] && arr[i][i] == arr[i + 2][i + 2] && arr[i][i] != ' ')
 			k = 1; break;
 	}
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++)
+	for (i = 0; i < row; i++) {
+		for (j = 0; j < col; j++)
 		{
 			if (arr[i][j] == ' ')
 				find = 0;
@@ -64,31 +77,39 @@ int judge(char arr[3][3]) {
 	return k;
 	
 }
+void isWin(int a,int cout) {
+	if (a == 1) {
+		if (cout % 2 == 0) {
+			printf("电脑赢了！\n");
+			exit();
+		}
+		else {
+			printf("恭喜你赢了赢了！\n");
+			exit();
+		}
+	}
+	if (a == 2) {
+		printf("电脑赢了！\n");
+		exit();
+	}
+	
+		
+}
 void game(){
-	int cout;
-	int jg = 0;
-	int location[2];
-	cout = 1;
-    char chess[3][3] = { {"   "},{"   "},{"   "} };
-	board(chess);
+	int cout=1;
+    char chess[ROW][COL] = { {"   "},{"   "},{"   "} };
+	board(chess,ROW,COL);
 	do
-	{//一次下棋，输出棋盘，然后cout下棋步数增加
-        play(location,chess,cout);
-		if (cout % 2 == 1) 
-			chess[location[0] - 1][location[1] - 1] = '*';
-		else
-			chess[location[0] - 1][location[1] - 1] = 'o';
-		board(chess);
-		cout++;	
-		jg = judge(chess);
-	} while (jg==0);
-	if (jg==1) {
-		if (cout % 2 == 1)printf("恭喜o棋赢了：\n");
-		else printf("恭喜*棋赢了：\n");	
-	}
-	else {
-		printf("打平了！\n");
-	}
+	{   play(chess,ROW,COL,cout);
+		board(chess,ROW,COL);
+		isWin(judge(chess,ROW,COL), cout);
+		computerplay(chess, ROW, COL);
+		board(chess, ROW, COL);
+		isWin(judge(chess, ROW, COL), cout);
+
+	} while (1);
+	
+	
 }
 void exit1() {
 	printf("退出");
