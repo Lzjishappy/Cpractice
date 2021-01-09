f85ee2c76dd0cb698484a72b7e9ed1b22bbd965c
lzj <1476673483@qq.com>
2021-01-10 00:15:03 +0800

    debug：数组越界导致死循环

---
+++ cross.c
@@ -0,0 +1,11 @@
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include<stdio.h>
+//int main() {
+//	int i = 0;
+//	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++) {
+//		printf("hehe%d", i);
+//		a[i] = 0;
+//	}
+//	return 0;
+//}数组越界导致死循环
